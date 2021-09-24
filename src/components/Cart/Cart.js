100d187f7cff56250a9726b61ba9ebbabbc829e8
babulakterfsd <babulakterfsd@gmail.com>
2021-09-25 02:29:24 +0600

    dollar sign added before amount

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -26,15 +26,15 @@ const Cart = ({cart}) => {
          <h5 className="text-success">Order Summary: </h5>
          <h5 className="text-muted"><small>items ordered: {totalQuantity}</small></h5>
 
-         <div className="d-flex justify-content-around"><small>products total price:</small><small>{productsTotalPrice.toFixed(2)}</small></div>
+         <div className="d-flex justify-content-around"><small>products total price:</small><small>${productsTotalPrice.toFixed(2)}</small></div>
 
-         <div className="d-flex justify-content-around"><small>shipping charge:</small><small>{shippingAndHandling.toFixed(2)}</small></div>
+         <div className="d-flex justify-content-around"><small>shipping charge:</small><small>${shippingAndHandling.toFixed(2)}</small></div>
 
-         <div className="d-flex justify-content-around"><small>total before tax:</small><small>{totalBeforeTax.toFixed(2)}</small></div>
+         <div className="d-flex justify-content-around"><small>total before tax:</small><small>${totalBeforeTax.toFixed(2)}</small></div>
 
-         <div className="d-flex justify-content-around"><small>estimated tax:</small><small>{estimatedTax.toFixed(2)}</small></div>
+         <div className="d-flex justify-content-around"><small>estimated tax:</small><small>${estimatedTax.toFixed(2)}</small></div>
          
-         <div className="d-flex justify-content-around"><h5 className="text-muted">Total Bill:</h5><h5 className="text-muted">{orderTotal.toFixed(2)}</h5></div>
+         <div className="d-flex justify-content-around"><h5 className="text-muted">Total Bill:</h5><h5 className="text-muted">${orderTotal.toFixed(2)}</h5></div>
 
          <button className="btn btn-success fw-bold px-3 px-lg-4 text-white mt-2">Review Your Order</button>
       </div>
