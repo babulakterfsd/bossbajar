130d83f721572489b7fdcc309cc866d3d08a5714
babulakterfsd <babulakterfsd@gmail.com>
2021-10-13 01:58:58 +0600

    env variable implemented to hide secret firebase data

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBq6TVNg4_TzpjY_xxqNh5u54u1mPWyGBU",
-  authDomain: "bossbajar-a742b.firebaseapp.com",
-  projectId: "bossbajar-a742b",
-  storageBucket: "bossbajar-a742b.appspot.com",
-  messagingSenderId: "4036304830",
-  appId: "1:4036304830:web:282b1137a5902829daa3ef",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
